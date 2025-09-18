d68519526db2193e23774c16a0a77afb12b51c8c
Bangdx1412 <doxuanbang14122005@gmail.com>
2025-09-18 22:30:37 +0700

    first commit

---
+++ resources/views/chat/chatpublic.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+@section('style')
+    
+@endsection
+<div class="container">
+    <div class="row justify-content-center">
+    </div>
+</div>
+@endsection
+@section('script')
+    
+@endsection
